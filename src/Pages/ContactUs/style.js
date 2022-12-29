66119dbc4b21a202bcafcd8c5a17829d196549b1
bouillot <Matthieu.Bouillot@etu.univ-grenoble-alpes.fr>
2022-12-29 16:26:07 +0100

    Ajout page de Contact

--- src/Pages/ContactUs/style.js
+++ src/Pages/ContactUs/style.js
@@ -3,6 +3,17 @@ import styled from "styled-components";
 
 export const StyledContactUs = styled.div`
 
+.section-header{
+    padding-bottom: 0px;
+    text-align: center;
+}
+
+
+.section-user-comment, .section-user-name, .section-user-mail{
+    margin-top: 10px;
+    width: 100%;
+}
+
     
     
 `
